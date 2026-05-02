f8c91aa35abdc472fb37cd308048b6df2a84ad34
MaVirgil <magnusvirgil@hotmail.com>
2026-05-02 13:31:01 +0200

    add generic exception handling

--- src/main/java/org/mavirgil/pixelsort/Exception/GlobalExceptionHandler.java
+++ src/main/java/org/mavirgil/pixelsort/Exception/GlobalExceptionHandler.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception e) {
+        return ResponseEntity.internalServerError().body("an unexpected error occurred");
     }
 }
